91a4e01259cc73af01be2374e929462e86946133
odoo-syria-team <153421050+odoo-syria-team@users.noreply.github.com>
2024-01-23 14:06:12 +0300

    Add files via upload
    return befor add cors

--- NWC_django_pro/NWC/settings.py
+++ NWC_django_pro/NWC/settings.py
@@ -41,13 +41,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'images',
-    'corsheaders',
+    # 'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'NWC.urls'
 
